4dc5e60538bee736503e741e99abd71d25ea95af
beepscore <support@beepscore.com>
2012-04-07 22:45:52 -0700

    In hash_with_count_test.rb add test method.

--- test/hash_with_count_test.rb
+++ test/hash_with_count_test.rb
@@ -30,4 +30,13 @@ class HashWithCountTest < MiniTest::Unit::TestCase
     pp actual_result
     assert_equal(expected_result, actual_result, 'hash with count empty should be empty')
   end
+  
+
+  def test_hash_with_count_two_one()
+    an_array = [:a, :a, :b]
+    expected_result = {:a => 2, :b => 1}
+    actual_result = HashWithCount.new(an_array)
+    pp actual_result
+    assert_equal(expected_result, actual_result, 'hash with count empty should be empty')
+  end
 end
