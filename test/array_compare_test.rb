bcffc3c2fe7161604b900ea52299841c21f00c1b
beepscore <support@beepscore.com>
2012-04-07 23:29:05 -0700

    In array_compare_test.rb add tests.

--- test/array_compare_test.rb
+++ test/array_compare_test.rb
@@ -6,4 +6,38 @@ require_relative '../lib/array_compare'
 class ArrayCompareTest < MiniTest::Unit::TestCase
 
 
+  def test_array_compare()
+    a = []
+    b = []
+    assert(a.is_equivalent?(b))
+    
+    a = [:a]
+    b = [:a]
+    assert(a.is_equivalent?(b))
+
+    a = [:b, :a]
+    b = [:b, :a]
+    assert(a.is_equivalent?(b))
+
+    a = [:a, :a, :b]
+    b = [:a, :b, :a]
+    assert(a.is_equivalent?(b))
+  end
+
+
+  def test_array_compare_false()
+    a = []
+    b = [:b]
+    assert(!a.is_equivalent?(b))
+    
+    a = [:a]
+    b = []
+    assert(!a.is_equivalent?(b))
+
+    a = [:a, :a, :b]
+    b = [:a, :b, :b]
+    assert(!a.is_equivalent?(b))
+
+  end
+
 end
