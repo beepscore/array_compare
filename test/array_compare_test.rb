d3c344f0961ff388656c9bf2e6edd336e16e34a9
beepscore <support@beepscore.com>
2012-04-08 00:16:15 -0700

    Add assertions, rename test methods.

--- test/array_compare_test.rb
+++ test/array_compare_test.rb
@@ -6,22 +6,40 @@ require_relative '../lib/array_compare'
 class ArrayCompareTest < MiniTest::Unit::TestCase
 
 
-  def test_array_compare()
+  def test_empty()
     a = []
     b = []
     assert(a.is_equivalent?(b))
-    
+  end
+
+  def test_equal() 
     a = [:a]
     b = [:a]
     assert(a.is_equivalent?(b))
 
-    a = [:b, :a]
-    b = [:b, :a]
+    a = [:b, :a, :f]
+    b = [:b, :a, :f]
     assert(a.is_equivalent?(b))
+  end
 
+  def test_different_duplicates()
     a = [:a, :a, :b]
-    b = [:a, :b, :a]
-    assert(a.is_equivalent?(b))
+    b = [:a, :b, :b]
+    assert(!a.is_equivalent?(b))
+  end
+
+  def test_different_length()
+    a = []
+    b = [:b]
+    assert(!a.is_equivalent?(b))
+
+    a = [:a]
+    b = []
+    assert(!a.is_equivalent?(b))
+
+    a = [1, -2, 3]
+    b = [1, -2, 3, -4]
+    assert(!a.is_equivalent?(b))
   end
 
   def test_multiple_types()
@@ -30,19 +48,20 @@ class ArrayCompareTest < MiniTest::Unit::TestCase
     assert(a.is_equivalent?(b))
   end
 
-  def test_array_compare_false()
-    a = []
-    b = [:b]
-    assert(!a.is_equivalent?(b))
-    
-    a = [:a]
-    b = []
+  def test_multiple_types_false()
+    a = [1, :a, :a, {}]
+    b = [{}, :a, 2, :a]
     assert(!a.is_equivalent?(b))
 
+    a = [1, :a, :a, {:c => 6}]
+    b = [{}, :a, 1, :a]
+    assert(!a.is_equivalent?(b))
+  end
+
+  def test_different_order()
     a = [:a, :a, :b]
-    b = [:a, :b, :b]
-    assert(!a.is_equivalent?(b))
-
+    b = [:a, :b, :a]
+    assert(a.is_equivalent?(b))
   end
 
 end
