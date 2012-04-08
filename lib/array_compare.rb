a852bdb7df709532a9bcd38f1228881e87b15cb1
beepscore <support@beepscore.com>
2012-04-07 23:28:43 -0700

    In array_compare.rb add method is_equivalent?

--- lib/array_compare.rb
+++ lib/array_compare.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 
+require_relative 'hash_with_count'
 require 'pp'
 
 class Array
 
+  def is_equivalent?(an_array)
+    is_equivalent = false
+    if ( (self.length == an_array.length) &&
+        (HashWithCount.new(self) == HashWithCount.new(an_array)) )
+      is_equivalent = true
+    end
+    is_equivalent
+  end
 
 end
